6a1fdd0f9717f117a9b8e3bd141d158b6a19b10a
moonkid <raylee@ieee.org>
2018-09-12 13:24:02 +1000

    haha

---
+++ Game.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using SplashKitSDK;
+
+/// <summary>
+/// Game Controller Class
+/// </summary>
+public class Game
+{
+    private Window _gameWindow;
+    private Wall _wall;
+    private Bat _nPCBat;
+    private Bat _playerBat;
+    private List<Ball> _balls;
+
+    private Font _font;
+    public enum State
+    {
+        
+    }
+
+}
